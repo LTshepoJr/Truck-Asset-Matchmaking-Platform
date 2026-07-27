7c74cc1b83cc070865f6bf8062fd0147c7787df1
Lethabo Kgoele <lethabotshepojr@gmail.com>
2026-07-27 18:21:17 +0200

    feat: add TransporterDashboardPage component with initial layout and description

---
+++ src/pages/transporter/TransporterDashboardPage.tsx
@@ -0,0 +1,12 @@
+export function TransporterDashboardPage() {
+  return (
+    <section>
+      <h2>Transporter Dashboard</h2>
+
+      <p>
+        From here, transporters will post available trucks, review suitable
+        loads and manage active trips.
+      </p>
+    </section>
+  );
+}
