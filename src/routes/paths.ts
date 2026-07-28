1c81dbab5e2e47ebab30399b9b125aae2b4f4496
Lethabo Kgoele <lethabotshepojr@gmail.com>
2026-07-29 01:30:25 +0200

    feat: add routes for ForgotPassword and ResetPassword

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -2,6 +2,10 @@ export const ROUTES = {
   home: "/",
   login: "/login",
   register: "/register",
+
+  forgotPassword: "/forgot-password",
+  resetPassword: "/reset-password",
+
   freightOwner: "/freight-owner",
   transporter: "/transporter",
   admin: "/admin",
